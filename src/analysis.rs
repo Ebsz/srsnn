1d3a01b890a8a4eb0201c127c69642b72bd3fbe7
Einar Sønju <einarsonju@gmail.com>
2024-06-20 01:02:12 +0200

    Implement more graph analysis

--- src/analysis.rs
+++ src/analysis.rs
@@ -10,6 +10,9 @@ use ndarray::{s, Axis, Array, Array1, Array2};
 
 use std::fmt;
 
+use petgraph::matrix_graph::MatrixGraph;
+use petgraph::algo::astar;
+
 
 pub struct Graph {
     pub rank: usize,
@@ -27,11 +30,28 @@ impl fmt::Display for Graph {
 impl<N> From<&NetworkRepresentation<N>> for Graph {
     fn from(item: &NetworkRepresentation<N>) -> Graph {
         Graph {
-            rank: item.edges().len(),
-            size: item.n,
+            rank: item.n,
+            size: item.edges().len(),
 
             matrix: item.connection_mask.clone()
         }
+    }
+}
+
+impl Into<MatrixGraph<(), ()>> for &Graph {
+    fn into(self) -> MatrixGraph<(),()> {
+        let mut edges = Vec::new();
+
+        for (i, d) in self.matrix.iter().enumerate() {
+            let x = i / self.rank;
+            let y = i % self.rank;
+
+            if *d == 1 {
+                edges.push((x as u16, y as u16));
+            }
+        }
+
+        MatrixGraph::<(), ()>::from_edges(edges)
     }
 }
 
@@ -39,26 +59,125 @@ pub struct GraphAnalysis {
     pub rank: usize,
     pub size: usize,
 
-    pub dg_dist: Array1<u32>, // Number of edges for each vertice
+    pub avg_degree: f32,
+    pub degree_dist: Array1<u32>, // Number of edges for each vertice
+
+    pub small_world_coefficient: f32,
+    pub clustering_coefficient: f32,
+    pub avg_path_len: f32,
 }
 
 impl GraphAnalysis {
     pub fn analyze(g: &Graph) -> GraphAnalysis {
+        let h = Self::avg_clustering(g);
+        let l = Self::avg_path_length(g);
+
+        let degree_dist = Self::degree_distribution(g);
+
+        let avg_degree = degree_dist.iter().sum::<u32>() as f32 / g.rank as f32;
 
         GraphAnalysis {
-            dg_dist: Self::degree_distribution(g),
             rank: g.rank,
-            size: g.size
+            size: g.size,
+
+            avg_degree,
+            degree_dist,
+
+            small_world_coefficient: h / l,
+            clustering_coefficient: h,
+            avg_path_len: l
         }
+    }
+
+    /// Calculate the small-world coefficient
+    pub fn small_world(g: &Graph) -> f32 {
+        let h = Self::avg_clustering(g);
+        let l = Self::avg_path_length(g);
+
+        h / l
     }
 
     fn degree_distribution(g: &Graph) -> Array1<u32> {
         g.matrix.sum_axis(Axis(1))
     }
 
-    /// Calculate the small-world coefficient \lambda = \frac{H}{L}
-    fn small_world(g: &Graph) {
+    fn avg_path_length(g: &Graph) -> f32 {
+        let mg: MatrixGraph<(),()> = g.into();
 
+        let mut dst = 0;
+
+        let mut n = 0;
+
+        for i in 0..g.rank {
+            for j in (i+1)..g.rank {
+                let a = Self::path_length(&mg, i as u16, j as u16);
+                let b = Self::path_length(&mg, j as u16, i as u16);
+
+                dst += a + b;
+            }
+        }
+
+        dst as f32 / (g.rank * (g.rank -1)) as f32
+    }
+
+    /// Calculate the shortest path from vertice s to vertice j,
+    /// returning 0 if no such path exists.
+    fn path_length(mg: &MatrixGraph<(),()>, s: u16, j: u16) -> u32 {
+        let path = astar(&mg, s.into(), |finish| finish == j.into(), |e| 1, |_| 0);
+
+        if let Some(res) = path {
+            return res.0;
+        }
+
+        0
+    }
+
+    fn avg_clustering(g: &Graph) -> f32 {
+        let sum_h: f32 = (0..g.rank).map(|i| Self::clustering(g, i)).sum();
+
+        sum_h / g.rank as f32
+    }
+
+    /// Single vertice clustering coefficient
+    fn clustering(g: &Graph, i: usize ) -> f32 {
+        let neighbors = Self::neighborhood(g, i);
+
+        let mut edges = 0;
+        for i in 0..neighbors.len() {
+            for j in (i+1)..neighbors.len() {
+                if g.matrix[[i, j]] == 1 {
+                    edges += 1;
+                }
+                if g.matrix[[j, i]] == 1 {
+                    edges += 1
+                }
+            }
+        }
+
+        if edges == 0 {
+            return 0.0;
+        }
+
+        let e_max = neighbors.len() * (neighbors.len() -1);
+        let h = e_max as f32 / edges as f32;
+
+        h
+    }
+
+    /// Vertices with edges to/from i
+    fn neighborhood(g: &Graph, i: usize) -> Vec<usize>{
+        let mut n: Vec<usize> = vec![];
+
+        n.extend(g.matrix.slice(s![i,..]).iter().enumerate()
+            .filter_map(|(n, x)| if *x == 1 && n != i {Some(n)} else { None }));
+
+        n.extend(g.matrix.slice(s![..,i]).iter().enumerate()
+            .filter_map(|(n, x)| if *x == 1 && n != i {Some(n)} else { None }));
+
+        n.sort();
+        n.dedup_by(|a, b| a == b);
+
+        n
     }
 }
 
