efb2598a2d75765d5f1ad13c4ffe167a94504884
Einar Sønju <einarsonju@gmail.com>
2024-10-16 16:07:31 +0200

    Update multipattern task

--- tasks/src/multipattern.rs
+++ tasks/src/multipattern.rs
@@ -8,27 +8,37 @@ use ndarray::{Array, Array1, Array2};
 use ndarray_rand::rand_distr::StandardNormal;
 
 
-//const DATASET_SIZE: usize = 1024;
+const N_CLASSES: usize = 2;
 
-const N_CLASSES: usize = 5;
+// dataset size = # of setups = N_CLASSES * N_TRIALS
+const N_TRIALS: usize = 32;
 
 const PATTERN_SIZE: usize = 5;
 const PATTERN_MAX_PROBABILITY: f32 = 0.3;
 
-const SEND_TIME: u32 = 50;          // How many timesteps to send the pattern
-const RESPONSE_DELAY: u32 = 43;     // Delay between pattern send and response window
-const RESPONSE_BIN_LEN: u32 = 50;
+// ------ Timing ------
+const SEND_TIME: u32 = 50;              // How many timesteps to send the pattern
+const RESPONSE_DELAY: u32 = 0; //42;   // Delay between pattern send and response window
+const RESPONSE_BIN_LEN: u32 = 40;
 
 const RESPONSE_WINDOW: u32 = RESPONSE_BIN_LEN * N_CLASSES as u32;  // Number of timesteps to record the response
 
 const RESPONSE_START_T: u32 = SEND_TIME + RESPONSE_DELAY;
 const MAX_T: u32 = SEND_TIME + RESPONSE_DELAY + RESPONSE_WINDOW;
 
+// ------ I/O --------
 const AGENT_INPUTS: usize = PATTERN_SIZE.pow(2);
-//const AGENT_OUTPUTS: usize = 25;
 
 const OUTPUTS_PER_CLASS: usize = 5;
 const AGENT_OUTPUTS: usize = N_CLASSES * OUTPUTS_PER_CLASS;
+
+
+// ------ Fitness -----
+const MAX_SPIKE_COUNT: u32 = RESPONSE_BIN_LEN * AGENT_OUTPUTS as u32;
+const MAX_SPIKERATE: f32 = 0.2; // Agent output spike frequency is capped above this, which avoids
+                                // the problem where maximum fitness = maximum spike count
+
+const SPIKE_CAP: f32 = MAX_SPIKE_COUNT as f32 * MAX_SPIKERATE;
 
 
 #[derive(Debug)]
@@ -123,8 +133,8 @@ impl TaskEval for MultiPatternTask {
         let mut setups = vec![];
 
         // 1. Static patterns
-        for _ in 0..10 {
-            for i in 0..5 {
+        for _ in 0..N_TRIALS {
+            for i in 0..N_CLASSES {
                 let dist: Array1<f32> = Array::from_iter(PATTERNS[i].iter())
                     .mapv(|x| *x) * PATTERN_MAX_PROBABILITY;
 
@@ -158,7 +168,13 @@ impl TaskEval for MultiPatternTask {
             let mut bin_count: Array1<f32> = r.response
                 .exact_chunks((RESPONSE_BIN_LEN as usize, AGENT_OUTPUTS))
                 .into_iter().map(|x| x.sum() as f32).collect();
+            //println!("total_bin_count: {}", bin_count);
 
+            // Apply spike rate cap
+            bin_count = bin_count.mapv(|x| math::minf(&[x, SPIKE_CAP]));
+            //println!("bin_count_post_cap: {}", bin_count);
+
+            // Subtract the max value
             bin_count = &bin_count - math::maxf(bin_count.as_slice().unwrap());
 
             // punish no-spike results to avoid getting stuck
@@ -172,13 +188,9 @@ impl TaskEval for MultiPatternTask {
             label[r.label] = 1.0;
 
             let loss = math::ml::cross_entropy(&predictions, &label);
+            //println!("bin_count: {}\nlabel: {}\npredictions: {}\nloss: {}\n\n", bin_count, label, predictions, loss);
 
             total_loss += loss;
-
-            //println!("{}", bin_count);
-            //println!("{}", label);
-            //println!("{}", predictions);
-            //println!("{}", loss);
         }
 
         100.0 - total_loss  // + Self::accuracy(&results).unwrap() * 50.0
@@ -194,11 +206,17 @@ impl TaskEval for MultiPatternTask {
                 .exact_chunks((RESPONSE_BIN_LEN as usize, AGENT_OUTPUTS))
                 .into_iter().map(|x| x.sum() as f32).collect();
 
+            // Apply spike rate cap
+            bin_count = bin_count.mapv(|x| math::minf(&[x, SPIKE_CAP]));
+
+            // Subtract the max value
             bin_count = &bin_count - math::maxf(bin_count.as_slice().unwrap());
 
             let predictions = math::ml::softmax(&bin_count);
 
             let predicted_label = math::max_index(predictions);
+
+            //log::info!("predicted: {}, label: {}", predicted_label, r.label);
 
             //println!("p: {predicted_label}, l: {}", r.label);
             if predicted_label == r.label {
@@ -322,24 +340,24 @@ pub fn pattern(n: usize, center: (usize, usize)) -> Array1<f32> {
     Array::from_iter(p.iter().cloned())
 }
 
-const PATTERNS: [[f32;25]; 5] = [
+pub const PATTERNS: [[f32;25]; 5] = [
 [0.0011561034, 0.06622559, 0.028740074, 0.038053624, 0.93971705,
 0.02895607, 0.020980915, 0.2684978, 0.074474305, 0.81641483,
 0.07613335, 0.03424606, 0.0040422105, 0.011101845, 0.7176192,
 0.32731244, 0.937556, 0.16226645, 0.6776351, 0.9081723,
 0.73409295, 0.9865458, 0.9638059, 0.9442252, 0.9574313],
 
-[0.5905768, 0.22691146, 0.060718138, 0.06451979, 0.010411645,
-0.29749846, 0.05148896, 0.0101640625, 0.056540046, 0.13277686,
-0.03487958, 0.5345852, 0.0022553147, 0.05475845, 0.2142955,
-0.23426689, 0.011564345, 0.050734133, 0.38911572, 0.14685844,
-0.0416429, 0.33551505, 0.03780432, 0.3593929, 0.6682871],
-
 [0.9953399, 0.97412777, 0.70939493, 0.7137156, 0.92732006,
 0.79044336, 0.8728952, 0.20001277, 0.35282508, 0.95879966,
 0.86484444, 0.12602703, 0.23220628, 0.19903651, 0.026103357,
 0.8509064, 0.060482394, 0.4071675, 0.5829529, 0.007789653,
 0.8824559, 0.7899793, 0.052196264, 0.00951018, 0.0077034584],
+
+[0.5905768, 0.22691146, 0.060718138, 0.06451979, 0.010411645,
+0.29749846, 0.05148896, 0.0101640625, 0.056540046, 0.13277686,
+0.03487958, 0.5345852, 0.0022553147, 0.05475845, 0.2142955,
+0.23426689, 0.011564345, 0.050734133, 0.38911572, 0.14685844,
+0.0416429, 0.33551505, 0.03780432, 0.3593929, 0.6682871],
 
 [0.42095488, 0.24399209, 0.9722555, 0.6680904, 0.92513806,
 0.16057067, 0.62162894, 0.2521128, 0.8902818, 0.96078885,
