58d1e5b94dd594992b13f38e42de9cb75087d86a
Einar Sønju <einarsonju@gmail.com>
2024-11-22 01:01:16 +0100

    Fix firing rate calculation

--- utils/src/analysis.rs
+++ utils/src/analysis.rs
@@ -8,14 +8,13 @@ pub fn firing_rate(spike_record: Array2<u32>, window: usize) -> Array2<f32> {
 
     let mut fr: Array2<f32> = Array::zeros(data.raw_dim());
 
-    for t in 0..max_t as i32 {
+    for t in 1..(max_t+1) as i32 {
         let t1 = std::cmp::max(t - window as i32, 0);
-        //println!("{}, {}", t1, t- );
 
         let d: Array1<f32> = data.slice(s!(t1..t, ..))
             .sum_axis(Axis(0)).mapv(|x| x as f32);
 
-        fr.slice_mut(s!(t,..)).assign(&d);
+        fr.slice_mut(s!(t-1,..)).assign(&d);
 
     }
 
